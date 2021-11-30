261026ad60d911dfb1783fed883be1ff5d4c0f7d
Chuntao han <chhan@redhat.com>
2021-11-30 17:52:46 +0800

    Not display drop-down file data in reports admin

--- pelc2/reports/admin.py
+++ pelc2/reports/admin.py
@@ -3,13 +3,16 @@ from django.contrib import admin
 from reports.models import LicenseDetection
 from reports.models import CopyrightDetection
 
+
 @admin.register(LicenseDetection)
 class LicenseDetectionAdmin(admin.ModelAdmin):
+    raw_id_fields = ('file',)
     list_display = ('file', 'license_key', 'detector', 'false_positive')
     search_fields = ['file__swhid', 'license_key', 'detector']
 
 
 @admin.register(CopyrightDetection)
 class CopyrightDetectionAdmin(admin.ModelAdmin):
+    raw_id_fields = ('file',)
     list_display = ('file', 'statement', 'detector', 'false_positive')
     search_fields = ['file__swhid', 'statement', 'detector']
