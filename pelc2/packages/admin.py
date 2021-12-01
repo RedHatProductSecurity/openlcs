f8ffeb1277f99ac9916a2062c99ba4915a88ecf9
Chuntao han <chhan@redhat.com>
2021-12-01 14:49:13 +0800

    Add Source, Path, and Package models in packages app

--- pelc2/packages/admin.py
+++ pelc2/packages/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from packages.models import File
+from packages.models import Path
+from packages.models import Source
+from packages.models import Package
 
 
 # Register your models here.
@@ -7,3 +10,23 @@ from packages.models import File
 class FileAdmin(admin.ModelAdmin):
     list_display = ('id', 'swhid')
     search_fields = ['id', 'swhid']
+
+
+@admin.register(Path)
+class PathAdmin(admin.ModelAdmin):
+    raw_id_fields = ('source', 'file')
+    list_display = ('source', 'file', 'path')
+    search_fields = ['source__name', 'file__swhid', 'path']
+
+
+@admin.register(Source)
+class SourceAdmin(admin.ModelAdmin):
+    list_display = ('checksum', 'name', 'url', 'state', 'archive_type')
+    search_fields = ['checksum', 'name', 'url', 'archive_type']
+
+
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    raw_id_fields = ('source',)
+    list_display = ('nvr', 'source', 'sum_license', 'is_source')
+    search_fields = ['nvr', 'source__name', 'sum_license']
--- pelc2/packages/models.py
+++ pelc2/packages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import URLValidator
 
 
 # Create your models here.
@@ -18,3 +19,104 @@ class File(models.Model):
 
     def __str__(self):
         return self.swhid
+
+
+class Source(models.Model):
+    """
+    Sources for source package information.
+    """
+    checksum = models.CharField(
+        max_length=50,
+        help_text='Checksum for this package'
+    )
+    name = models.CharField(
+        max_length=128,
+        help_text='Name of this package'
+    )
+    url = models.TextField(
+        blank=True, null=True,
+        verbose_name="URL",
+        validators=[URLValidator(message='Please enter a valid URL.')],
+        help_text='Upstream project URL'
+    )
+    state = models.IntegerField(
+        default=0,
+        help_text='Used to track package analysis status'
+    )
+    archive_type = models.CharField(
+        max_length=8,
+        help_text='Type of archive'
+    )
+
+    class Meta:
+        app_label = 'packages'
+
+    def __str__(self):
+        return self.name
+
+
+class Path(models.Model):
+    """
+    Path for files and sources.
+    """
+    source = models.ForeignKey(
+        Source,
+        on_delete=models.CASCADE,
+        help_text='Reference to source package'
+    )
+    file = models.ForeignKey(
+        File,
+        on_delete=models.CASCADE,
+        help_text='Reference to a file'
+    )
+    path = models.TextField(
+        help_text='File path within source package'
+    )
+
+    class Meta:
+        app_label = 'packages'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['source', 'file', 'path'],
+                name='unique_source_file_path'
+            ),
+        ]
+
+    def __str__(self):
+        return self.path
+
+
+class Package(models.Model):
+    """
+    Source files for a given archive.
+    """
+    nvr = models.CharField(
+        max_length=512,
+        verbose_name='NVR',
+        help_text='Package nvr'
+    )
+    source = models.ForeignKey(
+        Source,
+        on_delete=models.CASCADE,
+        help_text='Reference to source package'
+    )
+    sum_license = models.TextField(
+        null=True, blank=True,
+        help_text='File path within source package'
+    )
+    is_source = models.BooleanField(
+        default=False,
+        help_text='True if this package corresponds to the entire source, rather than an actual binary package' # noqa
+    )
+
+    class Meta:
+        app_label = 'packages'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['nvr', 'source'],
+                name='unique_nvr_source'
+            ),
+        ]
+
+    def __str__(self):
+        return self.nvr
