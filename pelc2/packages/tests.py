7b50ad7f8a8f39b7d0f5412a2fbdda1061f4be2e
Chuntao han <chhan@redhat.com>
2021-12-14 21:37:53 +0800

    Add unittest for packages file model

--- pelc2/packages/tests.py
+++ pelc2/packages/tests.py
@@ -1,1 +1,23 @@
+from django.test import TestCase
+from packages.models import File
+
+
 # Create your tests here.
+class FileModelTest(TestCase):
+    def setUp(self):
+        super(FileModelTest, self).setUp()
+        self.swhid = "123-456-789"
+        File.objects.create(swhid=self.swhid)
+
+    def test_file_model(self):
+        file = File.objects.get(id=1)
+        self.assertEqual(
+            file.swhid, self.swhid,
+            f'Cannot get the correct swhid {self.swhid}'
+        )
+
+        file_count = File.objects.filter(swhid=self.swhid).count()
+        self.assertEqual(
+            file_count, 1,
+            f'Should only exist 1 record with swhid {self.swhid}'
+        )
