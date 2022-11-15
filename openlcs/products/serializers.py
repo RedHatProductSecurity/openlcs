595ab0af5418b32bd1d1ed4ee503992303c0e31e
Qingmin Duanmu <qduanmu@redhat.com>
2022-11-15 21:47:32 +0800

    Add provides in component endpoints

--- openlcs/products/serializers.py
+++ openlcs/products/serializers.py
@@ -1,10 +1,9 @@
+from django.conf import settings
 from rest_framework import serializers
 
+from packages.models import Component
+from packages.serializers import ComponentSerializer
 from products.models import Product, Release
-from packages.serializers import (
-    ComponentSerializer,
-    GroupComponentsSerializer,
-)
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -33,33 +32,23 @@ class ReleaseSerializer(serializers.ModelSerializer):
             return retval
         # There should be only one root node for each release
         release_node = obj.release_nodes.get()
-        group_nodes = release_node.get_descendants().filter(
-            component__type__in=['OCI', 'RPMMOD']
-        )
-        # To avoid circular imports
-        from packages.models import Component
-        group_components = Component.objects.filter(
-            id__in=group_nodes.values_list('object_id', flat=True)
-        )
-        serializer = GroupComponentsSerializer(
-            group_components, many=True
-        )
-        retval.append(serializer.data)
+        descendant_nodes = release_node.get_descendants()
+
+        # Add container, module components into release data
+        nested_types = ['OCI', 'RPMMOD']
+        for component_type in nested_types:
+            nodes = descendant_nodes.filter(component__type=component_type)
+            components = Component.objects.filter(
+                id__in=nodes.values_list('object_id', flat=True)
+            )
+            serializer = ComponentSerializer(components, many=True)
+            retval.append(serializer.data)
+
+        all_types = settings.CORGI_COMPONENT_TYPES
+        other_types = [ct for ct in all_types if ct not in nested_types]
         # Note: `exclude` won't work for reverse generic relations
         # https://code.djangoproject.com/ticket/26261
-        other_nodes = release_node.get_descendants().filter(
-            component__type__in=[
-                "CARGO",
-                "GEM",
-                "GENERIC",
-                "GITHUB",
-                "GOLANG",
-                "MAVEN",
-                "NPM",
-                "RPM",
-                "PYPI"
-            ]
-        )
+        other_nodes = descendant_nodes.filter(component__type__in=other_types)
         other_components = Component.objects.filter(
             id__in=other_nodes.values_list('object_id', flat=True)
         )
