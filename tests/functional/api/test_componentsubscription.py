a4484200b4d9575294de29ee0a0f94d34a021246
Yuguang Wang <yuwang@redhat.com>
2023-01-19 14:01:18 +0800

    Add several test cases.

---
+++ tests/functional/api/test_componentsubscription.py
@@ -0,0 +1,45 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+import pytest
+
+from packages.models import ComponentSubscription
+
+
+def test_list_subscription(openlcs_client):
+    response = openlcs_client.api_call(
+        "/subscriptions/",
+        method="GET",
+        expected_code=status.HTTP_200_OK
+    )
+    assert response["count"] == 2
+    assert response["results"][0]["name"] == 'ansible_automation_platform:2.2'
+
+def test_create_subscription(openlcs_client):
+    subscription_name = "mock_subscription"
+    response = openlcs_client.api_call(
+        "/subscriptions/",
+        method="POST",
+        data={
+            "name": subscription_name,
+            "query_params": '{"arch": "src"}'
+        },
+        expected_code=status.HTTP_201_CREATED
+    )
+    instance = ComponentSubscription.objects.get(pk=response["id"])
+    assert instance.name == subscription_name
+    assert instance.query_params == {"arch": "src"}
+
+def test_patch_subscription(openlcs_client):
+    data = dict()
+    data['name'] = 'test_subscription_updated'
+    data['active'] = False
+    response = openlcs_client.api_call(
+        "/subscriptions/2/",
+        method="PATCH",
+        data=data,
+        expected_code=status.HTTP_200_OK
+    )
+    instance = ComponentSubscription.objects.get(pk=2)
+    assert instance.name == 'test_subscription_updated'
+    assert instance.active == False
