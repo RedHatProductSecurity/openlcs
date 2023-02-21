5c80d76ce1d0359729da29c0e41cd43d141533c0
Yuguang Wang <yuwang@redhat.com>
2023-02-21 23:57:12 +0800

    Fix task wrapper bug.
    There are chances the args[0] is not of dict type, it's up to the caller.

--- openlcsd/flow/task_wrapper.py
+++ openlcsd/flow/task_wrapper.py
@@ -9,24 +9,25 @@ class WorkflowWrapperTask(celery.Task):
 
     def after_return(self, status, retval, task_id, args, kwargs, einfo):
         """Handler called after the task returns."""
-        # Update the status of task when `duplicate_import` flag exists
-        duplicate_import = args[0].get('duplicate_import')
-        if status == 'SUCCESS' and duplicate_import:
-            self.update_state(state='DUPLICATE')
+        if isinstance(args[0], dict):
+            # Update the status of task when `duplicate_import` flag exists
+            duplicate_import = args[0].get('duplicate_import')
+            if status == 'SUCCESS' and duplicate_import:
+                self.update_state(state='DUPLICATE')
 
-        comp_type = args[0].get('component_type')
-        # Only keep uncompressed sources for container
-        if comp_type != 'OCI' or args[0].get('parent'):
-            src_dest_dir = args[0].get('src_dest_dir')
-            if src_dest_dir and os.path.exists(src_dest_dir):
-                delete(src_dest_dir)
-        # Only keep the source tarball for failed container components
-        if status == 'FAILURE' and 'src_dir' in args[0]:
-            pass
-        else:
-            tmp_src_filepath = args[0].get('tmp_src_filepath')
-            if tmp_src_filepath and os.path.exists(tmp_src_filepath):
-                delete(tmp_src_filepath)
+            comp_type = args[0].get('component_type')
+            # Only keep uncompressed sources for container
+            if comp_type != 'OCI' or args[0].get('parent'):
+                src_dest_dir = args[0].get('src_dest_dir')
+                if src_dest_dir and os.path.exists(src_dest_dir):
+                    delete(src_dest_dir)
+            # Only keep the source tarball for failed container components
+            if status == 'FAILURE' and 'src_dir' in args[0]:
+                pass
+            else:
+                tmp_src_filepath = args[0].get('tmp_src_filepath')
+                if tmp_src_filepath and os.path.exists(tmp_src_filepath):
+                    delete(tmp_src_filepath)
         super().after_return(
             status, retval, task_id, args, kwargs, einfo)
 
