36ea741aa687be63431e2e5c59e0d34f92192444
Yuguang Wang <yuwang@redhat.com>
2023-05-19 10:42:35 +0800

    Enforce expiration for acquired locks
    It is configured to be the hard time limit in celery, makes no
    sense to persist the lock after the limit is exceeded.

--- openlcs/libs/redis.py
+++ openlcs/libs/redis.py
@@ -4,7 +4,8 @@ from redis import Redis
 from redis_lock import Lock
 
 from openlcsd.celeryconfig import broker_url
-from libs.constants import TASK_IDENTITY_PREFIX  # noqa: E402
+from openlcsd.celeryconfig import task_time_limit
+from .constants import TASK_IDENTITY_PREFIX  # noqa: E402
 
 
 # Based upon singleton's lock generation mechanism with some simplification.
@@ -44,13 +45,35 @@ class RedisClient(object):
         # FIXME: raise exception if `broker_url` is improperly configured
         self.client = Redis.from_url(broker_url)
 
-    def get_lock(self, lock_key: str, lock_id=None):
+    def get_lock(self, lock_key: str, lock_id=None,
+                 expire: int = task_time_limit):
+        """Retrieve a lock object based on the provided lock_key and
+        optional lock_id.
+
+        Args:
+            lock_key (str): The key associated with the lock.
+            lock_id (str, optional): The unique identifier for the lock.
+                Defaults to None.
+            expire (int, optional): The expiration time for the lock in
+                seconds. Defaults to task_time_limit.
+
+        Returns:
+            redis_lock.Lock: A lock object with the provided parameters.
+        """
         if lock_id is not None:
-            return Lock(self.client, lock_key, id=lock_id)
-        return Lock(self.client, lock_key)
+            return Lock(self.client, lock_key, id=lock_id, expire=expire)
+        return Lock(self.client, lock_key, expire=expire)
 
     def release_lock_for_key(self, lock_key: str, lock_id=None) -> None:
+        """Release a lock specified by lock_key and optionally lock_id.
+
+        Args:
+            lock_key (str): key needed to initialize the lock
+            lock_id (str, optional): id attribute of the lock object. Useful
+                when you want to release the lock in a different place other
+                than where the lock is acquired. Defaults to None.
+        """
         lock = self.get_lock(lock_key)
-        # TODO: figure out why lock.release() won't work
-        # lock.release()
+        # FIXME: Forcibly deletes the lock. Need to figure out why
+        # lock.release() won't work.
         lock.reset()
