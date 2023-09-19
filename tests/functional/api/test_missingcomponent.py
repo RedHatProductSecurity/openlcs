b6a56477750e817517c25b1297552a2504bb6d91
Qingmin Duanmu <qduanmu@redhat.com>
2023-09-19 12:13:14 +0800

    Add API tests for missing components

---
+++ tests/functional/api/test_missingcomponent.py
@@ -0,0 +1,70 @@
+from django.conf import settings
+from rest_framework import status
+
+
+def test_list_missingcomponents(openlcs_client):
+    """
+    Test for listing missing components.
+    """
+    url = '/missingcomponents/?subscription_id=1'
+    response = openlcs_client.api_call(url, 'GET')
+    expected = [
+        {
+            "id": 1,
+            "purl": "pkg:golang/github.com/moby/sys/signal@v0.6.0",
+            "subscriptions": [
+                1
+            ]
+        }
+    ]
+    assert response.get("results") == expected
+
+    url = '/missingcomponents/?subscription_id=2'
+    response = openlcs_client.api_call(url, 'GET')
+    expected = []
+    assert response.get("results") == expected
+
+
+def test_retrieve_missingcomponent(openlcs_client):
+    """
+    Test for retrieving a missing component.
+    """
+    url = '/missingcomponents/1/'
+    response = openlcs_client.api_call(url, 'GET')
+    expected = {
+        "id": 1,
+        "purl": "pkg:golang/github.com/moby/sys/signal@v0.6.0",
+        "subscriptions": [
+            1
+        ]
+    }
+    assert response == expected
+
+
+def test_create_missingcomponents(openlcs_client):
+    """
+    Test for creating missing components.
+    """
+    url = '/missingcomponents/'
+    data = {
+        "missing_purls": ["pkg:rpm/389-admin@1.1.42"],
+        "subscription_id": 2
+    }
+    response = openlcs_client.api_call(
+        url,
+        method="POST",
+        data=data,
+        expected_code=status.HTTP_200_OK
+    )
+    url = '/missingcomponents/?subscription_id=2'
+    response = openlcs_client.api_call(url, 'GET')
+    expected = [
+        {
+            "id": 2,
+            "purl": "pkg:rpm/389-admin@1.1.42",
+            "subscriptions": [
+                2
+            ]
+        }
+    ]
+    assert response.get("results") == expected
